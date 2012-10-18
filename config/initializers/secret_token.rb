84ea5a43860c0f758edd2618dc1a9144d18d5766
Matt Carbone <matt.carbone@gmail.com>
2012-10-18 11:14:43 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrelloPlayground::Application.config.secret_token = '5d7d8ea1b59124be434394814c0f2730f2f6b0fc5b45eeb3c307aa963cb39c906d841a743ea9064d809540beef72bce52ad432427ba69a219114816134ea0afa'
